d4abc18c5d46a0ab8bec74cb232097194596ee57
pkulkarni <priyanka.k.1589@gmail.com>
2018-08-03 03:10:33 +0200

    mid part 2

---
+++ data.py
@@ -0,0 +1,14 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title':'Article One',
+            'body':'Lorem ipsum dolor sit amet'
+        },
+        {
+            'id': 2,
+            'title':'Article Two',
+            'body':'Lorem ipsum dolor sit'
+        }
+    ]
+    return articles

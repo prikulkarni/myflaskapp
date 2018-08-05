a2b954f8f6cebdebc6b2a0644bf601e79b7f4915
pkulkarni <priyanka.k.1589@gmail.com>
2018-08-05 22:13:38 +0200

    part 2 completed

--- app.py
+++ app.py
@@ -1,9 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for, request, session, logging
 from data import Articles
+from flask_sqlalchemy import SQLAlchemy
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
 
 app = Flask(__name__)
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:linux123@localhost/testdb'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'change'
+db = SQLAlchemy(app)
 Articles = Articles()
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False, unique=True)
+    username = db.Column(db.String, nullable=False, unique=True)
+    password = db.Column(db.String, nullable=False)
+
+    def __init__(self, name, email, username, password):
+        self.name =  name
+        self.email = email
+        self.username = username
+        self.password = password
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+
 
 @app.route('/')
 def index():
@@ -20,6 +44,40 @@ def articles():
 @app.route('/article/<string:id>/')
 def article(id):
     return render_template('article.html', id=id)
+
+class RegisterForm(Form):
+    name = StringField('Name', [validators.Length(min=1, max=50)])
+    email = StringField('Email', [validators.Length(min=6, max=50)])
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    password = PasswordField('Password', [
+    validators.DataRequired(),
+    validators.EqualTo('confirm', message='Passwords do not match')
+    ])
+    confirm = PasswordField('Confirm Password')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm(request.form)
+    if request.method == 'POST' and form.validate():
+        name = form.name.data
+        email = form.email.data
+        username = form.username.data
+        password = sha256_crypt.encrypt(str(form.password.data))
+        rec = User(name, email, username, password)
+        db.session.add(rec)
+        db.session.commit()
+
+        flash('Registered', 'success' )
+        return redirect(url_for('index'))
+    return render_template('register.html', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password_candidate = request.form['password']
+
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
